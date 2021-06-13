9c1ee508e921e209d2e399ea1d6a7b7e12539aa0
SaintZ <spaceghostpurp93@gmail.com>
2021-06-13 12:19:30 -0400

    gitignore: allow build script in source

---
+++ build_schwifty.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo
+echo "Clean Build Directory"
+echo 
+
+make clean && make mrproper
+
+echo
+echo "Issue Build Commands"
+echo
+
+mkdir -p out
+export ARCH=arm64
+export SUBARCH=arm64
+export CLANG_PATH=~/proton-clang/install/bin
+export PATH=${CLANG_PATH}:${PATH}
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=~/proton-clang/install/bin/aarch64-linux-gnu-
+export CROSS_COMPILE_ARM32=~/proton-clang/install/bin/arm-linux-gnueabi-
+export LD_LIBRARY_PATH=~/proton-clang/install/lib:$LD_LIBRARY_PATH
+
+echo
+echo "Set DEFCONFIG"
+echo 
+make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip O=out schwifty_defconfig
+
+echo
+echo "Build The Good Stuff"
+echo 
+
+make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip O=out -j16
