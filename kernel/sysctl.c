643ea1a8550760f7b024b0a2322099e027afe2c2
SaintZ <spaceghostpurp93@gmail.com>
2021-08-23 19:42:04 -0400

    Revert "mm: Refactor background writeback against synchronous writeback"
    This reverts commit a27028451206aab6af82acb3dd0157ba131157b8.

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -131,6 +131,7 @@ static int __maybe_unused two = 2;
 static int __maybe_unused three = 3;
 static int __maybe_unused four = 4;
 static unsigned long zero_ul;
+static unsigned long one_ul = 1;
 static unsigned long long_max = LONG_MAX;
 static int one_hundred = 100;
 static int one_thousand = 1000;
@@ -1491,6 +1492,14 @@ static struct ctl_table vm_table[] = {
 		.proc_handler	= dirty_background_ratio_handler,
 		.extra1		= &zero,
 		.extra2		= &one_hundred,
+	},
+	{
+		.procname	= "dirty_background_bytes",
+		.data		= &dirty_background_bytes,
+		.maxlen		= sizeof(dirty_background_bytes),
+		.mode		= 0644,
+		.proc_handler	= dirty_background_bytes_handler,
+		.extra1		= &one_ul,
 	},
 	{
 		.procname	= "dirty_ratio",
